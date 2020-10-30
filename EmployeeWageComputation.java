0ecf07417446eee680663a88593904064cffcf7c
subbus <subbareddyg83@gmail.com>
2020-10-30 18:28:39 +0530

    added employee attendance check

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,7 +2,16 @@ public class EmployeeWageComputation
 {
     public static void main(String args[])
     {
-        System.out.println("Welcome to Employee Wage Computation in Java");
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
 
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
     }
 }
