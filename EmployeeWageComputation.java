6019d6549d07bec74c3cff180acbbcf9b5a2b1de
subbus <subbareddyg83@gmail.com>
2020-10-31 20:54:36 +0530

    code is formatted

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -14,24 +14,23 @@ public class EmployeeWageComputation
         for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
                 && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
         {
-
+            
             int empType = (int) (Math.random() * 100) % 3;
             switch (empType)
             {
-            case FULL_TIME:
-                workingHrs = 8;
-                break;
-            case PART_TIME:
-                workingHrs = 4;
-                break;
-            default:
-                workingHrs = 0;
-                break;
+                case FULL_TIME:
+                    workingHrs = 8;
+                    break;
+                case PART_TIME:
+                    workingHrs = 4;
+                    break;
+                default:
+                    workingHrs = 0;
+                    break;
             }
             int wage = workingHrs * WAGE_PER_HR;
             totalWage += wage;
             System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
-
         }
         System.out.println("Total wage for a month is " + totalWage);
     }
