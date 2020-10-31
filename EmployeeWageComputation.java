1f37f21dde6da0b3ac306b7faadb2a9d5ca2a36a
subbus <subbareddyg83@gmail.com>
2020-10-31 10:20:26 +0530

    added switch case inplace of if statements

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -6,18 +6,19 @@ public class EmployeeWageComputation
         final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
         int empType = (int) (Math.random() * 100) % 3;
-        int workingHours = 0;
-        if (empType == FULL_TIME)
+        int workingHours =0;
+        switch (empType)
         {
-            System.out.println("Employee is Present Full time");
-            workingHours = 8;
-        } else if (empType == PART_TIME)
-        {
-            System.out.println("Employee is Present Part time");
-            workingHours = 4;
-        } else
-        {
-            System.out.println("Employee is Absent");
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");    
         }
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
