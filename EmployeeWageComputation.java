605e4c5718d27516c6e8752a1e8ee9d8342df85d
subbus <subbareddyg83@gmail.com>
2020-11-01 20:22:05 +0530

    added  ability to save the total wage for each company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,21 +1,36 @@
 public class EmployeeWageComputation
 {
-    public static void calculateTotalWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
-    {
-        final int PART_TIME = 1;
-        final int FULL_TIME = 2;
-        int totalWage = 0;
-        int workingHrs = 0;
+    // class constants
+    static final int PART_TIME = 1;
+    static final int FULL_TIME = 2;
+    // instance constants
+    final String COMPANY_NAME;
+    final int WAGE_PER_HR;
+    final int MAX_WORKING_DAYS;
+    final int MAX_WORKING_HRS;
+    // instance variable
+    int totalWage;
 
-        System.out.println("Details of " + companyName + " employee");
+    EmployeeWageComputation(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
+    {
+        COMPANY_NAME = companyName;
+        WAGE_PER_HR = wagePerHr;
+        MAX_WORKING_DAYS = maxWorkingDays;
+        MAX_WORKING_HRS = maxWorkingHrs;
+        totalWage = 0;
+    }
+    void calculateTotalWage()
+    {
+        int workingHrs = 0;
+        System.out.println("Details of " + COMPANY_NAME + " employee");
         System.out.println("-----------------------------------------------------");
-        System.err.println("Wage per hour:" + wagePerHr);
-        System.out.println("Maximum working days:" + maxWorkingDays);
-        System.out.println("Maximum working hours:" + maxWorkingHrs);
+        System.err.println("Wage per hour:" + WAGE_PER_HR);
+        System.out.println("Maximum working days:" + MAX_WORKING_DAYS);
+        System.out.println("Maximum working hours:" + MAX_WORKING_HRS);
         System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
 
-        for (int day = 1, totalWorkingHrs = 0; day <= maxWorkingDays
-                && totalWorkingHrs <= maxWorkingHrs; day++, totalWorkingHrs += workingHrs)
+        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
+                && totalWorkingHrs <= MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
         {
             int empType = (int) (Math.random() * 100) % 3;
             switch (empType)
@@ -30,15 +45,25 @@ public class EmployeeWageComputation
                     workingHrs = 0;
                     break;
             }
-            int wage = workingHrs * wagePerHr;
+            int wage = workingHrs * WAGE_PER_HR;
             totalWage += wage;
             System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
         }
-        System.out.println("Total wage for a month of " + companyName + " employee is " + totalWage + "\n");
+
+    }
+    public String toString()
+    {
+        return "Total wage for a month of " + COMPANY_NAME + " employee is " + totalWage + "\n";
     }
     public static void main(String args[])
     {
-        calculateTotalWage("Amazon", 40, 15, 200);
-        calculateTotalWage("BigBazar", 20, 20, 100);
+        EmployeeWageComputation google = new EmployeeWageComputation("Google", 8, 20, 100);
+        EmployeeWageComputation microsoft = new EmployeeWageComputation("Microsoft", 4, 30, 150);
+
+        google.calculateTotalWage();
+        System.out.println(google);
+
+        microsoft.calculateTotalWage();
+        System.out.println(microsoft);
     }
 }
